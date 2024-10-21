3e2ca98d1a01edcc1ef2ea8ed7235d918eb44cb3
OyShan1 <ilhamdzaky2007@gmail.com>
2024-10-21 01:26:44 +0000

    bud

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
-
 <?php
-session_start();
 
-if(!isset($_SESSION['email'])){
-    // header('location: ../loginn.php');
-    //po opo
-}
+include('navbar.php');
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <body>
-
-<?php
-// Variabel sesi gema yang ditetapkan pada halaman sebelumnya
-echo "Login sebagai " . $_SESSION [ "nama_lengkap" ] . ".<br>" ;
-echo "Emailku " . $_SESSION [ "email" ] . "." ;
-?>
-
+    <div class="page-content">
+        <p>p</p>
+    </div>
 </body>
 </html>
-
-<center><h1>ARIJUALFLYOVERJANTI</h1></center>
